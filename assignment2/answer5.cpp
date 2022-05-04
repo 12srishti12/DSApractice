d4b26868c2e04f9ee3c055abb6b20a4335227f5d
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-04 20:51:12 +0530

    Create answer5.cpp

---
+++ assignment2/answer5.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+     if(n==1){
+        return 0;
+    }
+    else{
+        for(int i=2;i<=sqrt(n);i++){
+            if(n%i==0){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+    }
+    return 1;
+}
+int main() {
+    for(int i=1;i<30;i++){
+         if (isPrime(i)){
+            cout<<"the number "<<i<<" is prime\n";
+         }
+         else{
+            cout<<"the number "<<i<<" is not prime\n";
+         }   
+    }
+}
