5799d8f71879d7621a47b1f361edad27c2e04f85
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-05 09:33:31 +0530

    Create arithmeticprogression.cpp

---
+++ arithmeticprogression.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>S;
+void ap(int a,int n,int d){
+	int ar;
+	
+	S.push_back(a);
+	for(int i=a;i<n;i++){
+		a=a+d;
+		S.push_back(a);
+	}
+	for(int i=0;i<n;i++){
+		cout<<S[i]<<" ";
+	}
+}
+
+int main() {
+	// your code goes here
+	int a,n,d,k;
+	cin>>a>>n>>d;
+	ap(a,n,d);
+	
+	return 0;
+}
