709453884cb650f9a8857d759701e4b0348750cd
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-10 09:57:30 +0530

    Create matrix_multi_using_array.cpp

---
+++ classroom/matrix_multi_using_array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[3][3],b[3][3],c[3][3];
+    cout<<"enter data in first 3X3 matrix\n";
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>a[i][j];
+        }
+    }
+    cout<<"enter data in second 3X3 matrix\n";
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>b[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<c[i][j]<<" ";
+        }
+    }
+    
+}
