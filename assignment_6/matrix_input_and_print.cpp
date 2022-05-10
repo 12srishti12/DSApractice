cd11e2f6f9c2f2e542c0c616bee1bc6a47394ed8
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-11 00:47:47 +0530

    Create matrix_input_and_print.cpp

---
+++ assignment_6/matrix_input_and_print.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
