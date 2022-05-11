c6eda19dbf205bb46079bd72330a3c443decb4e3
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-11 15:32:33 +0530

    Create sum_of_prime_factors.cpp

---
+++ assignment_5/sum_of_prime_factors.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+vector<int>prime;
+int primefact=0;
+void sieve(){
+    int max=2e5+5;
+    vector<bool>isprime(max,1);
+    isprime[0]=0;
+    for(int i=2;i<max;i++){
+        if(!isprime[i]) continue;
+        prime.push_back(i);
+        for(int j=i*i;j<max;j=j+i) isprime[j]=0;
+    }
+}
+
+signed main(){
+    int n;
+    cin>>n;
+    sieve();
+    for(int i=0;i<n;i++){
+        if(n%prime[i]==0){
+           primefact=primefact+prime[i]; 
+        }
+    }
+    cout<<primefact;
+}
