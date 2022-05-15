30436b81226c4edd918b27b7e1047f7a5f5a8fbf
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-15 16:31:53 +0530

    Create correctness.cpp

---
+++ Assignment_7/correctness.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int N, int arr[]) {
+    int i,j;
+    int value;
+    for(i=1;i<N;i++)
+    {
+        value=arr[i];
+        j=i-1;
+        while(j>=0 && value<arr[j])
+        {
+            swap(arr[j],arr[j+1]);
+            j=j-1;
+        }
+        value=arr[j+1];
+    }
+    for(j=0;j<N;j++)
+    {
+        printf("%d",arr[j]);
+        printf(" ");
+    }
+}
+int main(void) {
+
+    int N;
+    scanf("%d", &N);
+    int arr[N], i;
+    for(i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(N, arr);
+
+    return 0;
+}
