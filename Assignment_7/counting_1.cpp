f53b2085a5d248f211269e719d90cef1ee6f7326
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-15 20:23:32 +0530

    Create counting_1.cpp

---
+++ Assignment_7/counting_1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    vector<int>a;
+    a.resize(100,0);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        a[m]++;
+    }
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    
+    
+}
