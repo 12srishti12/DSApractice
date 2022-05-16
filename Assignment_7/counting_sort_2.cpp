fc34799be2c01b6dabe9978bdebc75efdc89fc90
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-05-16 10:47:57 +0530

    Create counting_sort_2.cpp

---
+++ Assignment_7/counting_sort_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    vector<int>a(100,0);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        a[m]++;
+    }
+    for(int i=0;i<a.size();i++){
+        for(int j=0;j<a[i];j++){
+            cout<<i<<" ";
+        }
+    }
+}
