f29ead04029825248b090aa53db0ac720c37dcf7
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-06-05 12:07:16 +0530

    Create Remove_linked_list_elements.cpp

---
+++ assignment_12/Remove_linked_list_elements.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+       ListNode *temp,*prev,*temp1;
+        prev=temp=head;
+        //int pos=1;
+        while(temp!=NULL){
+            if(temp->val==val && temp==head){
+                prev=temp;
+                temp=temp->next;
+                head=temp;
+                delete prev;
+                //pos++;
+            }
+            else if(temp->val!=val){
+                prev=temp;
+                temp=temp->next;
+                //pos++;
+            } 
+            else {
+                if(temp->next!=NULL){
+                    prev->next=temp->next;
+                    temp1=temp;
+                    temp=temp->next;
+                    delete temp1;
+                }
+                else{
+                    temp1=temp;
+                    delete temp1;
+                    prev->next=NULL;
+                    temp=prev;
+                }
+           }
+            }
+            
+      return head; 
+    }
+};
