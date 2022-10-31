894cc0b5e7eb17dc3b0f673f36547ade1e0582c6
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-10-31 21:46:09 +0530

    Create encrption.cpp

---
+++ assignment_23/encrption.cpp
@@ -0,0 +1,22 @@
+string encryption(string s) {
+    vector<string>v;
+    string k="",r="",z="";
+    int count=0;
+    for(char a:s){
+        if(a==' ') continue;
+        else{
+            k=k+a;
+            count++;
+        }
+        
+    }
+    cout<<k<<"\n";
+    cout<<count<<"\n";
+    int i=0,j=0;
+    while(){
+        if()
+    }
+    return r;
+}
+
+//incomplete
