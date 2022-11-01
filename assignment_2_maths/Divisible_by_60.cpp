1cc52c05cc4b97f0a749c58cec87872dee868726
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-11-01 21:32:27 +0530

    Update Divisible_by_60.cpp

--- assignment_2_maths/Divisible_by_60.cpp
+++ assignment_2_maths/Divisible_by_60.cpp
@@ -1,21 +1,20 @@
-int to_find(vector<int> B){
-    string k="";
-    for(int i=0;i<B.size();i++){
-        k=k+to_string(B[i]);
+int permut(vector<int>a){
+    int s=0;
+    for(int i=0;i<a.size();i++){
+        s=s*10+a[i];
     }
-    int p;
-    p=stoi(k);
-    return p;
+    return s;
 }
+
+
+
 int Solution::divisibleBy60(vector<int> &A) {
-    // string k="";
-    // for(int i=0;i<A.size();i++){
-    //     k=k+to_string(A[i]);
-    // }
-    // int p;
-    // p=stoi(k);
-    int s= to_find(A);
-    if(s%60==0) return 1;
-    if(next_permutation(A.begin(),A.end())) divisibleBy60(A);
+    if(permut(A)%60==0) return 1;
+    while(next_permutation(A.begin(),A.end())){
+        //next_permutation(A.begin(),A.end());
+        permut(A);
+        if(permut(A)%60==0) return 1;
+    }
+    
     return 0;
 }
