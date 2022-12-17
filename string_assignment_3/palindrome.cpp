1ae0408dfd312cf14aac6ec62c3ff7eb64e293ee
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2022-12-17 12:34:29 +0530

    Update palindrome.cpp

--- string_assignment_3/palindrome.cpp
+++ string_assignment_3/palindrome.cpp
@@ -17,3 +17,24 @@ int Solution::isPalindrome(string A) {
     }
     return 1;
 }
+// Corect solution
+
+int Solution::isPalindrome(string A) {
+    int i=0,j=A.size()-1;
+    while(i<j){
+        if(!isalnum(A[i])){
+            i++;
+            continue;
+        }
+        if(!isalnum(A[j])){
+            j--;
+            continue;
+        }
+        A[i]=tolower(A[i]);
+        A[j]=tolower(A[j]);
+        if(A[i]!=A[j]) return 0;
+        i++;
+        j--;
+    }
+    return 1;
+}
